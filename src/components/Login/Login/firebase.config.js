d87a48a7cd4bade3b90769ae24f901b56c9193be
Asaduzzaman Sajeeb <50229812+Asaduzzaman607@users.noreply.github.com>
2020-10-13 12:50:46 +0600

    doctors portal last version

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDJOz5TEcMF2JdE0rRvEtt87PEkCMkFgrQ",
+  authDomain: "mern-doctors-portal.firebaseapp.com",
+  databaseURL: "https://mern-doctors-portal.firebaseio.com",
+  projectId: "mern-doctors-portal",
+  storageBucket: "mern-doctors-portal.appspot.com",
+  messagingSenderId: "772272573846",
+  appId: "1:772272573846:web:2bc4844116f1d8380861b7"
+};
+  export default firebaseConfig;
